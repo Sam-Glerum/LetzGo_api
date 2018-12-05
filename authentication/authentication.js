e7c33200fbf63f6f83778129bc0793f4b3224d35
Sam Glerum <samuelglerum@hotmail.com>
2018-12-05 14:22:32 +0100

    added function to encode and decode jwt-tokens

---
+++ authentication/authentication.js
@@ -0,0 +1,37 @@
+const moment = require('moment');
+const jwt = require('jwt-simple');
+const config = require('../config/config');
+
+// Encode (username to token)
+function encodeToken(username) {
+    const payload = {
+        exp: moment().add(10, 'days').unix(),
+        iat: moment().unix(),
+        sub: username
+    };
+
+    return jwt.encode(payload, config.secretKey, null, null)
+}
+
+// Decode (token to username)
+function decodeToken(token, cb) {
+    try {
+        const payload = jwt.decode(token, config.secretKey, null, null);
+
+        const now = moment.now();
+
+        // Check if the token is expired
+        if (now > payload.exp) {
+            console.log('Token has expired');
+        }
+
+        cb(null, payload);
+    } catch (error) {
+        cb(error, null);
+    }
+}
+
+module.exports = {
+    encodeToken,
+    decodeToken
+};
