887d73f6364c9356ff87660e389b463ad500feea
Sam Glerum <samuelglerum@hotmail.com>
2018-12-07 00:52:42 +0100

    created createConcert method

---
+++ data/repository/concertRepo.js
@@ -0,0 +1,47 @@
+const Concert = require('../schema/concert');
+const jsonModel = require('../../models/response/JsonModel');
+const apiErrors = require('../../models/error/apiErrors');
+
+module.exports = class concertRepo {
+
+    static createConcert(nameParam, dateParam, cityParam, streetParam, houseNumberParam,
+                         zipCodeParam, priceParam, descriptionParam, artistParam, res) {
+        const url = "/api/concerts";
+        const httpMethod = "POST";
+
+        Concert.findOne({name: nameParam})
+            .then((concert) => {
+                if (concert === null ) {
+                    const newConcert = new Concert({
+
+                    });
+
+                    newConcert.save()
+                        .then((concert) => {
+                            res.status(201).json(new jsonModel(
+                                url,
+                                httpMethod,
+                                201,
+                                "Concert " + concert.name + " has been created"));
+                        })
+                        .catch(() => {
+                            res.status(500).json(new jsonModel(
+                                url,
+                                httpMethod,
+                                500,
+                                "Something went wrong. Concert " + concert.name + " has not been created."));
+                        })
+                } else {
+                    res.status(409).json(new jsonModel(url, httpMethod, 409, "Concert already exists."))
+                }
+            })
+            .catch(() => {
+                res.status(500).json(new jsonModel(
+                    url,
+                    httpMethod,
+                    500,
+                    "Something went wrong, please try again."
+                ))
+            })
+    }
+};
