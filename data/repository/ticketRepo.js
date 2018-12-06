ef661d642d06853222b1f29b00c10d20c6c75041
Sam Glerum <samuelglerum@hotmail.com>
2018-12-07 00:41:17 +0100

    added imports and createTicket method to ticketRepo

---
+++ data/repository/ticketRepo.js
@@ -0,0 +1,8 @@
+const Ticket = require('../schema/ticket');
+const jsonModel = require('../../models/response/JsonModel');
+const apiErrors = require('../../models/error/apiErrors');
+
+module.exports = class ticketRepo {
+
+    static createTicket();
+}
