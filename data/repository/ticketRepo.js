93d378709b96cbb7ae1382ce07604fb7f8bd7b5e
Sam Glerum <samuelglerum@hotmail.com>
2018-12-07 20:05:11 +0100

    created ticketRepo class. Added addTicket method

---
+++ data/repository/ticketRepo.js
@@ -0,0 +1,12 @@
+const Ticket = require('../schema/ticket');
+const jsonModel = require('../../models/response/JsonModel');
+
+module.exports = class ticketRepo {
+
+    static addTicket(concertId, userId, res) {
+        const url = "/api/tickets";
+        const httpMethod = "POST";
+
+
+    }
+};
