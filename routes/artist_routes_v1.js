23a8cd196d2b6e9bb6d7fa847fc694074ad23aa5
Sam Glerum <samuelglerum@hotmail.com>
2019-03-11 16:30:35 +0100

    created route for updating artist

--- routes/artist_routes_v1.js
+++ routes/artist_routes_v1.js
@@ -2,15 +2,19 @@ const express = require('express');
 const router = express.Router();
 const artistRepo = require('../data/repository/artistRepo');
 
+// Get all artists from db
 router.get('/', (req, res) => {
     artistRepo.getAllArtists(res);
 });
 
+
+// Get single artist from db
 router.get('/:artistId', (req, res) => {
     let artistId = req.params.artistId;
     artistRepo.getArtistById(artistId, res);
 });
 
+// Add a new artist to the artist DB
 router.post('/', (req, res) => {
     let artistInfo = req.body;
 
@@ -22,9 +26,13 @@ router.post('/', (req, res) => {
         artistInfo.discography,
         res
     );
+});
 
+// Update an existing artist in the db
+router.put('/:artistId', (req, res) => {
+    let artistInfo = req.body;
 
-
+    artistRepo.updateArtist();
 });
 
 module.exports = router;
