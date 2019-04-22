9b73fa360ffe9e6cea1629d24fe8c21eeddc95a4
Sam Glerum <samuelglerum@hotmail.com>
2019-04-22 15:07:25 +0200

    changed cors setting

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ server.use(bodyparser.json());
 
 //CORS headers
 server.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN || 'https://letzgo-frontend.herokuapp.com/' || 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN || 'https://letzgo.herokuapp.com/' || 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, x-access-token');
     res.setHeader('Access-Control-Allow-Credentials', true);
